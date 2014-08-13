7a3d02f5821ab271372d9ee5979cab2711de64f0
Hima <himat15@yahoo.com>
2014-08-13 16:26:19 -0400

    Stripe updates

--- src/com/nistica/tk/StripeOrder.java
+++ src/com/nistica/tk/StripeOrder.java
@@ -4,29 +4,65 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.stripe.Stripe;
+import com.stripe.exception.APIConnectionException;
+import com.stripe.exception.APIException;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import com.stripe.model.Customer;
 
 public class StripeOrder {
 	private Map<String, Object> chargeMap = new HashMap<String, Object>(); 
 	private Map<String, Object> cardMap = new HashMap<String, Object>();
+	private Map<String, Object> customerMap = new HashMap<String, Object>();
 	boolean cardSet = false;
+
+	public StripeOrder(){
+		Stripe.apiKey = "sk_test_4aGesuqZhhrj4LhMeV9d0EiM";
+	}
 	
 	
-	public void setCreditCard(String cardNumber, String personName, int expMonth, int expYear){
+	public boolean setCreditCard(String cardNumber, String personName, int expMonth, int expYear){
 		cardMap.put("number", cardNumber);
 		cardMap.put("name", personName);
 		cardMap.put("exp_month", expMonth);
 		cardMap.put("exp_year", expYear);
-		
+		cardMap.put("cvc", 233333);
 		
 		chargeMap.put("card", cardMap);
 		cardSet = true;
+		
+		customerMap.put("card", cardMap);
+		customerMap.put("description", personName);
+		try {
+			Customer customer = Customer.create(customerMap);
+			System.out.println("Customer id: " + customer.getId());
+		} catch (AuthenticationException e) {
+			System.out.println("AUTH EXCEPTION ON CUSTOMER");
+			e.printStackTrace();
+		} catch (InvalidRequestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (APIConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CardException e) {
+			System.out.println("CARD EXCEPTION");
+			e.printStackTrace();
+		} catch (APIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return true;
+		
 	}
 	
 	//remove static later
 	public boolean sendPayment(int amount, String desc){
-		Stripe.apiKey = "sk_test_4aGesuqZhhrj4LhMeV9d0EiM";
+		
 		
 		if(!cardSet)
 		{
--- src/com/nistica/tk/StripeTester.java
+++ src/com/nistica/tk/StripeTester.java
@@ -3,7 +3,9 @@ package com.nistica.tk;
 public class StripeTester {
 	public static void main(String[] args){
 		StripeOrder stripeOrder = new StripeOrder();
-		stripeOrder.setCreditCard("4242424242424242", "Person Name", 12, 2020);
+		
+		boolean bob = stripeOrder.setCreditCard("4242424242424242", "Person Name", 12, 2020);
+		System.out.println(bob);
 		stripeOrder.sendPayment(100, "<name> is ordering <food>");
-		}
+	}
 }
