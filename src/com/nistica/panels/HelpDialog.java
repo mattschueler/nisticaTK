d8cc823778f4abe4101eb9150ed97ff2edb9f947
mattschueler <mjschueler@gmail.com>
2014-08-19 09:52:59 -0400

    Fixed help dialog
    Old information still on help dialog, fixed it

--- src/com/nistica/panels/HelpDialog.java
+++ src/com/nistica/panels/HelpDialog.java
@@ -19,13 +19,13 @@ public class HelpDialog extends JDialog{
 		
 		JPanel helpPanel = new JPanel();
 		JTextArea helpInfo = new JTextArea(
-				"Help on using the Ordering System:\nTo add an item to your cart (right side), double click an item on the menu (left side).\nYou can " +
+				"Help on using the Ordering System:\nTo add an item to your cart (right side), click on the arrow of the item on the menu (left side).\nYou can " +
 	    		"adjust the parameters of your order at any time, on either side of the screen, but the parameters in your cart are the ones that will " +
 	    		"be saved.\nTo remove an item from your cart, simply click the x button on the right of each item.\nIf you add an item on the menu that is already in " + 
-	    		"your cart, the item in the cart will be overwritten.\nWhen you are ready to submit your order, hit the order button to review your order.\n" +
-	    		"If you are happy with your order, click the next order button to submit your order.  If you are not, simply close the window and make any changes " +
-	    		"you wish to make.\nYou can also hit the clear button to clear out your entire cart and start from the beginning.\nHover over an item to see its description."+
-	    		"\n\nThank you for using the Nistica Thai Kitchen Ordering System!"
+	    		"your cart, the item in the cart will be overwritten.\nWhen you are ready to submit your order, hit the order button to review your order.  The subtotal, tax, "+
+	    		"and total are all written at the bottom of the order popup\nIf you are happy with your order, click the next order button to submit your order.  If you are not, " +
+	    		"simply close the window and make any changes you wish to make.\nYou can also hit the clear button to clear out your entire cart and start from the beginning.\n" +
+	    		"Hover over an item to see its description.\n\nThank you for using the Nistica Thai Kitchen Ordering System!"
 	    		);
 		helpInfo.setLineWrap(true);
 		helpInfo.setWrapStyleWord(true);
